49b67536d3e446e7df9315a4686f3a25896c9b2d
beriguetemisael <beriguetemisael@gmail.com>
2025-10-14 23:45:04 +0000

    commit

--- card-backend/server.js
+++ card-backend/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-let cards = [];
-let idCounter = 1;
+const cardsFilePath = './cards.json';
+
+// Function to read cards from the JSON file
+const readCardsFromFile = () => {
+  try {
+    const data = fs.readFileSync(cardsFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+};
+
+// Function to write cards to the JSON file
+const writeCardsToFile = (cards) => {
+  fs.writeFileSync(cardsFilePath, JSON.stringify(cards, null, 2), 'utf8');
+};
+
+let cards = readCardsFromFile();
+let idCounter = cards.length > 0 ? Math.max(...cards.map(c => c.id)) + 1 : 1;
 
 app.get('/cards', (req, res) => {
   res.status(200).json(cards);
@@ -36,6 +54,7 @@ app.post('/cards', (req, res) => {
     name
   };
   cards.push(newCard);
+  writeCardsToFile(cards);
   res.status(201).json(newCard);
 });
 
@@ -48,6 +67,7 @@ app.put('/cards/:id', (req, res) => {
   if (number) card.number = number;
   if (exp) card.exp = exp;
   if (name) card.name = name;
+  writeCardsToFile(cards);
   res.status(200).json(card);
 });
 
@@ -55,6 +75,7 @@ app.delete('/cards/:id', (req, res) => {
   const index = cards.findIndex(c => c.id === parseInt(req.params.id));
   if (index !== -1) {
     cards.splice(index, 1);
+    writeCardsToFile(cards);
     res.status(204).send();
   } else {
     res.status(404).json({ message: 'Card not found' });
